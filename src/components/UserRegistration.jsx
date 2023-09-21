5ddad72c474bd57eb339095c69ea063632527eb0
jason <elijahchurch@gmail.com>
2023-09-21 16:28:39 -0700

    add register  and signout functionality

--- src/components/UserRegistration.jsx
+++ src/components/UserRegistration.jsx
@@ -1,26 +1,69 @@
 // import React from "react";
+import { useState } from "react"
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut} from "firebase/auth"
+import { auth } from "./../firebaseConfig"
 
 function AccountManagement(){
+    const [registerEmail, setRegisterEmail] = useState("")
+    const [registerPassword, setRegisterPassword] = useState("")
+    const [logInEmail, setLogInEmail] = useState ("")
+    const [logInPassword, setLogInPassword] = useState("")
+
+    //grabs the current user that is logged in
+    const [user, setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser)
+    })
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+            console.log(user)
+        }
+        catch(error) {
+            console.log(error.message)
+        }
+    };
+
+    const logIn = async () => {
+        
+    };
+
+    const logOut = async () => {
+        await signOut(auth)
+    };
+
+
     return(
         <div className="accountManagement">
-        <div className="userRegistration">
-            <h3> Register User</h3>
-            <input type="email" placeholder="Email..."/>
-            <input type="password" placeholder="Password..." />
+            <div className="userRegistration">
+                <h3> Register User</h3>
+                <input type="email" placeholder="Email..." onChange={(event) => {
+                    setRegisterEmail(event.target.value);
+                }}/>
+                <input type="password" placeholder="Password..." onChange={(event) => {
+                    setRegisterPassword(event.target.value);
+                }}/>
 
-            <button>Create user</button>
-        </div>
-        <div className="userLogin">
-            <h3>Login</h3>
-            <input type="email" placeholder="Email..."/>
-            <input type="password" placeholder="Password..." />
+                <button onClick={register}>Create user</button>
+            </div>
+            <div className="userLogin">
+                <h3>Login</h3>
+                <input type="email" placeholder="Email..." onChange={(event) => {
+                    setLogInEmail(event.target.value);
+                }}/>
+                <input type="password" placeholder="Password..." onChange={(event) => {
+                    setLogInPassword(event.target.value);
+                }}/>
 
-            <button>Login</button>
-        </div>
-        <div className="userSignOut">
-            <h3>User logged in</h3>
-            <button>Sign out</button>
-        </div>
+                <button>Login</button>
+            </div>
+            <div className="userSignOut">
+                <h3>User logged in:</h3>
+                {user?.email}
+                <button onClick={logOut}>Sign out</button>
+            </div>
         </div>
     )
 }
--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "AIzaSyCgCnAFq9kBNrp5fJ1i1204QakAJuH9hG4",
     authDomain: import.meta.env.TZREACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
