fb343a2cb837ce8561f71bfbcb2da75e8cca07aa
ellehailu <hailu.elle@gmail.com>
2023-10-03 11:57:21 -0700

    add "add contestant" functionality to Admin UI

--- src/components/AdminUI.jsx
+++ src/components/AdminUI.jsx
@@ -19,7 +19,60 @@ function AdminUI() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [bipSelections, setBipSelections] = useState([]);
     const [gbSelections, setGbSelections] = useState([]);
+    const [bipContestantInfo, setBipContestantInfo] = useState({
+        name:'',
+        pastAppearance:'',
+        photo: '',
+        status: '',
+        EpOneScore: 0,
+        EpTwoScore: 0,
+        EpThreeScore: 0,
+        EpFourScore: 0,
+        EpFiveScore: 0,
+        EpSixScore: 0,
+        EpSevenScore: 0,
+        EpEightScore: 0,
+        EpNineScore: 0,
+        EpTenScore: 0,
+        EpElevenScore: 0,
+        EpTwelveScore: 0
+    })
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setBipContestantInfo({...bipContestantInfo, [name]: value })
+    };
+
+    const handleStatusChange = (e) => {
+        setBipContestantInfo({...bipContestantInfo, status: e.target.value });
+    };
+
+   const handleNewBipSubmit = async(e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('https://localhost:5001/api/Bip', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(bipContestantInfo)
+            });
+
+            if (response.ok){
+                console.log("new contestant successfully added")
+                
+                return<h3>BIP contestant successful added</h3>
+            }
+            else{
+                console.error("failed to add new bip contestant")
+                return<h3>Failed to add new BIP contestant</h3>
+            }
+        }
+        catch (error){
+            console.error('Error', error)
+        }
+   }
 
     useEffect(() => {
         fetch(`https://localhost:5001/api/Bip`)
@@ -74,29 +127,159 @@ function AdminUI() {
         // display name on a flipable card that shows the bio and photo when user hovers over the name
     console.log("reached 'else' branch")
         return (
-        <div className="cardContainer">
-        <button>Bachelor in Paradise</button>
-        <button>Golden Bachelor</button>
-            <h1>BIP Contestants</h1>
-                <div className="contestantCard">
+        <div>
+            <div>
+                <h1>BIP Contestants</h1>
+                <h3>Add new Bip Contestant</h3>
+                <form onSubmit={handleNewBipSubmit}>
+                    <input 
+                    type="string" 
+                    name="name"
+                    placeholder="name" 
+                    value={bipContestantInfo.name}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="string" 
+                    name="pastAppearance"
+                    placeholder="Past Appearance" 
+                    value={bipContestantInfo.pastAppearance}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="string" 
+                    name="photo"
+                    placeholder="photo url" 
+                    value={bipContestantInfo.photo}
+                    onChange={handleChange}>
+                    </input>
+                    <div className="statusRadioButton">
+                        <label>
+                            <input
+                            type="radio"
+                            name="status"
+                            value="Active"
+                            checked={bipContestantInfo.status === 'Active'}
+                            onChange={handleStatusChange}/>Active
+                        </label>
+                        <label>
+                            <input
+                            type="radio"
+                            name="status"
+                            value="InActive"
+                            checked={bipContestantInfo.status === 'Active'}
+                            onChange={handleStatusChange}/>InActive
+                        </label>
+                    </div>
+                    <input 
+                    type="number" 
+                    name="epOneScore"
+                    placeholder="Episode one score" 
+                    value={bipContestantInfo.epOneScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epTwoScore"
+                    placeholder="Episode two score" 
+                    value={bipContestantInfo.epTwoScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epThreeScore"
+                    placeholder="Episode Three score" 
+                    value={bipContestantInfo.epThreeScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epFourScore"
+                    placeholder="Episode Four score" 
+                    value={bipContestantInfo.epFourScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epFiveScore"
+                    placeholder="Episode Five score" 
+                    value={bipContestantInfo.epFiveScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epSixScore"
+                    placeholder="Episode Six score" 
+                    value={bipContestantInfo.epSixScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epSevenScore"
+                    placeholder="Episode Seven score" 
+                    value={bipContestantInfo.epSevenScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epEightScore"
+                    placeholder="Episode Eight score" 
+                    value={bipContestantInfo.epEightScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epNineScore"
+                    placeholder="Episode Nine score" 
+                    value={bipContestantInfo.epNineScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epTenScore"
+                    placeholder="Episode Ten score" 
+                    value={bipContestantInfo.epTenScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epElevenScore"
+                    placeholder="Episode Eleven score" 
+                    value={bipContestantInfo.epElevenScore}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="number" 
+                    name="epTweleveScore"
+                    placeholder="Episode Tweleve score" 
+                    value={bipContestantInfo.epTweleveScore}
+                    onChange={handleChange}>
+                    </input>
+                    <button type="submit">Add new Bip Contestant</button>
+                </form>
+            </div>
+            
+
+            
+                <div>
                 <ul>
-                {bipSelections.map((bipContestants, index) =>
-                <li key={index}>
-                <h3>{bipContestants.name}</h3>
-                </li>)}
-            </ul>
-        </div>
-
-
-        <h1>GB Contestants</h1>
-        <div className="contestantCard">
-            <ul>
-                {gbSelections.map((gbContestants, index) =>
+                    {bipSelections.map((bipContestants, index) =>
                     <li key={index}>
-                        <h3>{gbContestants.name}</h3>
-            </li>)}
-            </ul>
-        </div>
+                    <h3>{bipContestants.name}</h3>
+                    </li>)}
+                </ul>
+            </div>
+
+
+            <h1>GB Contestants</h1>
+            <div>
+                <ul>
+                    {gbSelections.map((gbContestants, index) =>
+                        <li key={index}>
+                            <h3>{gbContestants.name}</h3>
+                </li>)}
+                </ul>
+            </div>
         </div>
         )
     }
