ea83962dced07a7905b0ad37626fd7e115890e3f
ellehailu <hailu.elle@gmail.com>
2023-10-05 09:49:44 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";
+import useUser from "../hooks/UseUser";
 
 function Header(){
 
+const [loggedIn, user] = useUser()
 //create signout function to be called when signout button is clicked in nav bar
 const logOut = async () => {
     await signOut(auth);
@@ -11,13 +13,12 @@ const logOut = async () => {
 
 
  return(
-        <div className="header">
-            <button className="headerButton" href="/">Home</button>
-            <button className="headerButton">
-                 Other Seasons</button> 
-            <button className="headerButton">Leaderboard</button>
-            <button className="headerButton">Weekly draft</button>
+    <div>
+
+    
+        {user && <div className="header">
             <button className="headerButton" onClick={logOut}>SignOut</button>
+        </div>}
         </div>
     )
 }
